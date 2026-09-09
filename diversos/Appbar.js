507f8ce9a3fee60a912a97a5efce107dd45a10fd
Mariana Barbosa de Lima <nutsoyasumi@gmail.com>
2026-09-09 16:56:52 -0300

    Cria appbar

---
+++ diversos/Appbar.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Appbar } from 'react-native-paper';
+
+const Appbar = () => (
+  <Appbar.Header>
+    <Appbar.Content title="Savvr" />
+  </Appbar.Header>
+);
+
+export default Appbar;
